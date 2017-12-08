f4f9388855a23851d2fb871be9b2c2ba79ccd8aa
Dylan Grafmyre <thorsummoner@live.com>
2017-12-07 22:31:08 -0800

    removing redundant garbage
    deleting more garbage
    
    garbage

--- tools/generate_trunk_locale_templates.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dir=`dirname $0`
-localedir="$dir/../trunk/locale"
-gladefile="$dir/../trunk/gui.glade"
-echo -n "Generating locale template in $podir " 
-xgettext --sort-output --keyword=translatable -o "$localedir/template.pot" "$gladefile"
-echo "
-[ DONE ]"
